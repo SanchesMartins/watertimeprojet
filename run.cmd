1dc16dc6ab30186f7a958397a45996635a7489b4
Gabriel Sanches Martins <gabriel.martins3@raizen.com>
2023-10-25 23:55:14 -0300

    Fix run.cmd, adjust .gitignore

--- run.cmd
+++ run.cmd
@@ -1,3 +1,17 @@
-cd .\src
+@echo off
+
+if NOT [%1] == [] goto setvar
+if [%1] == [] goto setauto
+
+:setvar
+set watertimehome=%1
+goto run
+
+:setauto:
+set watertimehome=./src
+goto run
+
+:run
+echo %watertimehome%
+cd %watertimehome%
 pythonw main.py >> ./logs/logs
-exit
